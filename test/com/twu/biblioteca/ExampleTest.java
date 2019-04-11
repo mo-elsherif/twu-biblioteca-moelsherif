c4e23056cfbdccccfda27372d2bc2211983c1562
Mohamed El Sherif <mohamed.elsherif@Mohameds-MacBook-Pro.local>
2019-04-11 13:06:13 +0200

    Adding Failing Test For Story 1

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,6 +1,6 @@
 package com.twu.biblioteca;
 
-
+import java.io.*;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -10,4 +10,23 @@ public class ExampleTest {
     public void test() {
         assertEquals(1, 1);
     }
-}
+
+
+        private static final String EOL =
+                System.getProperty("line.separator");
+
+        @Test
+        public void testShowUsageWhenInsufficientArgumentsSupplied() {
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            PrintStream console = System.out;
+            try {
+                System.setOut(new PrintStream(bytes));
+                BibliotecaApp.main();
+            } finally {
+                System.setOut(console);
+            }
+            assertEquals(String.format(
+                    "Welcome to Bibliotheca. Your one-stop-shop for great book titles in Bangalore\n"),
+                    bytes.toString());
+        }
+    }
