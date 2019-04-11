177fdc5c15ed0daecd784b07f0abba11e227cd73
Mohamed El Sherif <mohamed.elsherif@Mohameds-MacBook-Pro.local>
2019-04-11 13:11:09 +0200

    Renamed test

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,7 +1,9 @@
 package com.twu.biblioteca;
 
 import java.io.*;
+
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
@@ -12,21 +14,21 @@ public class ExampleTest {
     }
 
 
-        private static final String EOL =
-                System.getProperty("line.separator");
+    private static final String EOL =
+            System.getProperty("line.separator");
 
-        @Test
-        public void testShowUsageWhenInsufficientArgumentsSupplied() {
-            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-            PrintStream console = System.out;
-            try {
-                System.setOut(new PrintStream(bytes));
-                BibliotecaApp.main();
-            } finally {
-                System.setOut(console);
-            }
-            assertEquals(String.format(
-                    "Welcome to Bibliotheca. Your one-stop-shop for great book titles in Bangalore\n"),
-                    bytes.toString());
+    @Test
+    public void testBibliothecaGreetingMessage() {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        PrintStream console = System.out;
+        try {
+            System.setOut(new PrintStream(bytes));
+            BibliotecaApp.main();
+        } finally {
+            System.setOut(console);
         }
+        assertEquals(String.format(
+                "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore\n"),
+                bytes.toString());
     }
+}
